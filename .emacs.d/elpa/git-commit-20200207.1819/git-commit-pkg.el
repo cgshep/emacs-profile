a903b304501ca8f664a266c94ccf48b19289cd66
Carlton Shepherd <carlton@linux.com>
2022-01-31 10:28:50 +0000

    Cleanup .emacs.d

--- .emacs.d/elpa/git-commit-20200207.1819/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200207.1819" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "82d522928ce3a4da1d3104074f2954666095a317" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
